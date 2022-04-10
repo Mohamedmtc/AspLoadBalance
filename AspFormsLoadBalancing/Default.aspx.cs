5d45f17017f23797d3f4df09ef19982a577f175c
Mohamed Ebrahim Atia <m.attia@intdv.com>
2022-04-11 00:37:37 +0200

    first add1

---
+++ AspFormsLoadBalancing/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspFormsLoadBalancing
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if((System.Web.HttpContext.Current.User != null) && System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                Session["SharedValue"] = System.Web.HttpContext.Current.User.Identity.Name + DateTime.Now.ToString();
+            }
+            else
+            {
+                Session["SharedValue"] = Request.LogonUserIdentity.Name + DateTime.Now.ToString();
+            }
+           
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if(Session["SharedValue"]!=null)
+            Label1.Text="Session Value : " + Session["SharedValue"].ToString();
+        }
+    }
+}
---
+++ AspFormsLoadBalancing/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspFormsLoadBalancing.Startup))]
+namespace AspFormsLoadBalancing
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
